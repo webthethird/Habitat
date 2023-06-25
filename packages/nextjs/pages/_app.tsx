9e5057fbead3d5715b8452d539bf5882da868dec
Shubh Agarwal <mailtoshubh@gmail.com>
2023-06-24 20:14:10 -0400

    second commit

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -6,14 +6,14 @@ import NextNProgress from "nextjs-progressbar";
 import { Toaster } from "react-hot-toast";
 import { useDarkMode } from "usehooks-ts";
 import { WagmiConfig } from "wagmi";
-import { Footer } from "~~/components/Footer";
-import { Header } from "~~/components/Header";
-import { BlockieAvatar } from "~~/components/scaffold-eth";
-import { useNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
-import { useGlobalState } from "~~/services/store/store";
-import { wagmiClient } from "~~/services/web3/wagmiClient";
-import { appChains } from "~~/services/web3/wagmiConnectors";
-import "~~/styles/globals.css";
+import { Footer } from "/components/Footer";
+import { Header } from "/components/Header";
+import { BlockieAvatar } from "/components/scaffold-eth";
+import { useNativeCurrencyPrice } from "/hooks/scaffold-eth";
+import { useGlobalState } from "/services/store/store";
+import { wagmiClient } from "/services/web3/wagmiClient";
+import { appChains } from "/services/web3/wagmiConnectors";
+import "/styles/globals.css";
 
 const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
   const price = useNativeCurrencyPrice();
@@ -21,12 +21,6 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
   // This variable is required for initial client side rendering of correct theme for RainbowKit
   const [isDarkTheme, setIsDarkTheme] = useState(true);
   const { isDarkMode } = useDarkMode();
-
-  useEffect(() => {
-    if (price > 0) {
-      setNativeCurrencyPrice(price);
-    }
-  }, [setNativeCurrencyPrice, price]);
 
   useEffect(() => {
     setIsDarkTheme(isDarkMode);
