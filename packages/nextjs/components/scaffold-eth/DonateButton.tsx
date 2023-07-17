eef0e5025313ce99e1acb8052c263c083e9e46e3
webthethird <ratzbodell@gmail.com>
2023-07-16 22:26:23 -0500

    Update DonateButton with working attestation

--- packages/nextjs/components/scaffold-eth/DonateButton.tsx
+++ packages/nextjs/components/scaffold-eth/DonateButton.tsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
-import { EAS, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
+import { EAS, SchemaEncoder, SchemaRecord, SchemaRegistry } from "@ethereum-attestation-service/eas-sdk";
 import { ethers } from "ethers";
+import invariant from "tiny-invariant";
 import {
   useAccount,
   /* useNetwork, useSendTransaction, usePrepareSendTransaction, */
   useProvider,
+  useSigner,
 } from "wagmi";
 // import { hardhat, localhost, sepolia } from "wagmi/chains";
 // import { BanknotesIcon } from "@heroicons/react/24/outline";
-// import {
+import {
 //   useAccountBalance,
-//   useTransactor,
-// } from "~~/hooks/scaffold-eth";
+  useTransactor,
+} from "~~/hooks/scaffold-eth";
+import { useAccountModal } from "@rainbow-me/rainbowkit";
 
 // import { getLocalProvider } from "~~/utils/scaffold-eth";
 
@@ -22,57 +25,73 @@ import {
  */
 export const DonateButton = () => {
   const [isClicked, setIsClicked] = useState(false);
-  const { address } = useAccount();
+  const { address, status } = useAccount();
+  const { data: signer } = useSigner();
   const provider = useProvider();
   // const { balance } = useAccountBalance(address);
   // const { chain: ConnectedChain } = useNetwork();
   const [loading, setLoading] = useState(false);
-//   const faucetTxn = useTransactor();
+  const faucetTxn = useTransactor();
   const eas = new EAS("0xC2679fBD37d54388Ce493F1DB75320D236e1815e");
   eas.connect(provider);
 
   // Initialize SchemaEncoder with the schema string
   const schemaEncoder = new SchemaEncoder(
-    "address donation_from, address donation_to, bytes32 donation_tx, uint256 donation_value",
+    "address donation_to,address donation_from,bytes32 donation_tx,uint256 donation_val",
   );
 
   const sendETH = async () => {
-    setIsClicked(true);
-    try {
-    //   setLoading(true);
-    //   await faucetTxn({
-    //     to: "0x750EF1D7a0b4Ab1c97B7A623D7917CcEb5ea779C",
-    //     value: ethers.utils.parseEther("0.01"),
-    //   });
-      setLoading(false);
-      const encodedData = schemaEncoder.encodeData([
-        { name: "donation_to", value: "0x750EF1D7a0b4Ab1c97B7A623D7917CcEb5ea779C", type: "address" },
-        { name: "donation_from", value: address ? address : "", type: "address" },
-        {
-          name: "donation_tx",
-          value: "0x750EF1D7a0b4Ab1c97B7A623D7917CcEb5ea779C000000000000000000000000",
-          type: "bytes32",
-        },
-        { name: "donation_value", value: ethers.utils.parseEther("0.01"), type: "uint256" },
-      ]);
-      const schemaUID = "0x3bb54e554f4bba20b4c98584863a60985e4e021536311c3cf1798b6158015979";
-      const tx = await eas.attest({
-        schema: schemaUID,
-        data: {
-          recipient: address ? address.toString() : "",
-          expirationTime: 0,
-          revocable: true,
-          data: encodedData,
-        },
-      });
+    if (status !== "connected" || !address) {
+        useAccountModal();
+    } else {
+        invariant(signer, "signer must be defined");
+        eas.connect(signer);
 
-      const newAttestationUID = await tx.wait();
+        setIsClicked(true);
+        try {
+            setLoading(true);
+            let txResponse = await faucetTxn({
+                to: "0x750EF1D7a0b4Ab1c97B7A623D7917CcEb5ea779C",
+                value: ethers.utils.parseEther("0.1"),
+            });
+            if (!txResponse) {
+                console.log("Transaction errored or was canceled!");
+                return
+            }
+            const txHash = txResponse.hash;
+            setLoading(false);
+            const encodedData = schemaEncoder.encodeData([
+                { name: "donation_to", value: "0x750EF1D7a0b4Ab1c97B7A623D7917CcEb5ea779C", type: "address" },
+                { name: "donation_from", value: address, type: "address" },
+                {
+                name: "donation_tx",
+                value: txHash,
+                type: "bytes32",
+                },
+                { name: "donation_val", value: ethers.utils.parseEther("0.1"), type: "uint256" },
+            ]);
+            
+            const schemaUID = "0xadfffada54293ee92b824d9c271e9fca450cbf55634d69cbcda01c4efef45c90";
 
-      console.log("New attestation UID:", newAttestationUID);
-    } catch (error) {
-      console.error("⚡️ ~ file: FaucetButton.tsx:sendETH ~ error", error);
-      setLoading(false);
+            const tx = await eas.attest({
+                schema: schemaUID,
+                data: {
+                    recipient: address,
+                    expirationTime: 0,
+                    revocable: false,
+                    data: encodedData,
+                },
+            });
+
+            const newAttestationUID = await tx.wait();
+
+            console.log("New attestation UID:", newAttestationUID);
+        } catch (error) {
+            console.error("⚡️ ~ file: FaucetButton.tsx:sendETH ~ error", error);
+            setLoading(false);
+        }
     }
+    
   };
 
   const buttonStyle = {
@@ -96,7 +115,7 @@ export const DonateButton = () => {
     <button
       style={buttonStyle}
       onClick={() => {
-        !loading ? sendETH : console.log("Loading, please wait...");
+        !loading ? sendETH() : console.log("Loading, please wait...");
       }}
     >
       Donate
